4bb6df6681578bcdcc7322031e8b0289b2eb6309
Helio Dourado <heliohdd@yahoo.com.br>
2024-01-02 19:08:35 -0300

    02.21-criando-metodos-de-callback-do-ciclo-de-vida-dos-beans

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -10,10 +10,6 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail REAL");
-	}
-
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,19 +1,28 @@
 package com.algaworks.algafood.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
-@Component
+/*@Component*/
 public class AtivacaoClienteService {
 
 	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+
+	/* @PostConstruct */
+	private void init() {
+		System.out.println("INIT " + notificador);
+	}
+
+	/* @PreDestroy */
+	private void destroy() {
+		System.out.println("DESTROY");
+	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
