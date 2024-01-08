2159dc9fd8874649896ca45e28492f48c67c3f2a
Helio Dourado <heliohdd@yahoo.com.br>
2024-01-08 14:06:23 -0300

    03.09-adicionando-um-objeto-no-banco-de-dados

--- src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
@@ -25,5 +26,11 @@ public class CadastroCozinha {
 		
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
+	}
+	
+	@Transactional
+	public Cozinha adicionar(Cozinha cozinha) {
+	// insert into cozinha (nome) values (...)
+		return manager.merge(cozinha);
 	}
 }
