b063b626b51078c5f153525af57fdef31c5d69c5
Helio Dourado <heliohdd@yahoo.com.br>
2024-01-31 09:03:37 -0300

    05.08-conhecendo-os-prefixos-de-query-methods

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -1,17 +1,26 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
-import java.math.BigDecimal;
-import java.util.List;
 
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 
-	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+//	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
 	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante> findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long id);
 }
