ffb935e93c47fee8e025e660ff3f4ca273065b1e
Helio Dourado <heliohdd@yahoo.com.br>
2024-01-31 12:42:06 -0300

    05.10-externalizando-consultas-jpql-para-um-arquivo-xml

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -18,7 +17,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 //	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 	
 //	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
