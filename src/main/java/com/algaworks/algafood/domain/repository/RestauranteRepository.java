09cb589846737a0d3ef9ea6be6607085c4043d18
Helio Dourado <heliohdd@yahoo.com.br>
2024-01-31 07:49:35 -0300

    05.07-usando-as-keywords-para-definir-criterios-de-query-methods

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -4,8 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import java.math.BigDecimal;
+import java.util.List;
+
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
+
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 }
