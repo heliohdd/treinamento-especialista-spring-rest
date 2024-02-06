2c39f4e1b4342d48414d3c80d4403cc260873a8c
Helio Dourado <heliohdd@yahoo.com.br>
2024-02-06 09:51:49 -0300

    06.03-analisando-o-impacto-do-relacionamento-muitos-para-muitos-na-rest-api

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -68,7 +68,8 @@ public class RestauranteController {
 			Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).orElse(null);
 
 			if (restauranteAtual != null) {
-				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				BeanUtils.copyProperties(restaurante, restauranteAtual, 
+						"id", "formasPagamento");
 
 				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
 				return ResponseEntity.ok(restauranteAtual);
