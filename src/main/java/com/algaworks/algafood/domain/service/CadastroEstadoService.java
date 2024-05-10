f5cc820fd4924b28b366b759a16d2a282761782a
Helio Dourado <heliohdd@yahoo.com.br>
2024-05-10 12:04:14 -0300

    08.10-afinando-a-granularidade-e-definindo-a-hierarquia-das-exceptions-de-negocios

--- src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -6,7 +6,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 
@@ -15,9 +15,6 @@ public class CadastroEstadoService {
 
 	private static final String MSG_ESTADO_EM_USO = 
 			"Estado de código %d não pode ser removido, pois está em uso";
-	
-	private static final String MSG_ESTADO_NAO_ENCONTRADO = 
-			"Não existe um cadastro de estado com código %d";
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
@@ -29,9 +26,10 @@ public class CadastroEstadoService {
 	public void excluir(Long estadoId) {
 		try {
 			estadoRepository.deleteById(estadoId);
+		
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId));
+			throw new EstadoNaoEncontradoException(estadoId); 
+		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
 					String.format(MSG_ESTADO_EM_USO, estadoId));
@@ -40,7 +38,6 @@ public class CadastroEstadoService {
 	
 	public Estado buscarOuFalhar(Long estadoId) {
 		return estadoRepository.findById(estadoId)
-				.orElseThrow(() -> new EntidadeNaoEncontradaException(
-						String.format(MSG_ESTADO_NAO_ENCONTRADO, estadoId)));
+				.orElseThrow(() -> new EstadoNaoEncontradoException(estadoId));
 	}
 }
