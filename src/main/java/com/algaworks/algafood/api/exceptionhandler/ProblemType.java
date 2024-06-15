655b46411ff848389f6e4f1ca781a2dc02a35c4e
Helio Dourado <heliohdd@yahoo.com.br>
2024-06-15 10:10:09 -0300

    08.27-desafio-tratando-outras-excecoes-nao-capturadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -9,7 +9,8 @@ public enum ProblemType {
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
-	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
 	
 	private String title;
 	private String uri;
