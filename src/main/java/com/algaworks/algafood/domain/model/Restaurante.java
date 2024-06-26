0f909110f5516f95f08e21582c8e1a7b3a4b83db
Helio Dourado <heliohdd@yahoo.com.br>
2024-06-26 11:31:06 -0300

    09.10-customizando-mensagens-de-validacao-na-anotacao-da-constraint

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -43,7 +43,7 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank
+	@NotBlank(message = "Nome é obrigatório")
 	@Column(nullable = false)
 	private String nome;
 
