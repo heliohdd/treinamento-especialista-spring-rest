c6b14eff172b8170552682de8280930e2b0b908e
Helio Dourado <heliohdd@yahoo.com.br>
2024-06-26 14:43:55 -0300

    09.11-customizando-e-resolvendo-mensagens-de-validacao-globais-em-resource-bundle

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -43,7 +43,7 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(message = "Nome é obrigatório")
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
