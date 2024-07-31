e810317012704367fe95168fd1953241a5a1e308
Helio Dourado <heliohdd@yahoo.com.br>
2024-07-31 16:17:20 -0300

    11.20-desafio-usando-dtos-como-representation-model

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -22,13 +22,11 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class Cozinha {
 
-//	@NotNull(groups = Groups.CozinhaId.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 
