6ad389a7489e636791b760b98c2814776e997539
Helio Dourado <heliohdd@yahoo.com.br>
2024-09-26 15:16:13 -0300

    13.01-fazendo-projecao-de-recursos-com-jsonview-do-jackson

--- src/main/java/com/algaworks/algafood/api/assembler/CozinhaModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/CozinhaModelAssembler.java
@@ -7,8 +7,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.domain.model.Cozinha;
-import com.algaworks.algafood.domain.model.CozinhaModel;
 
 @Component
 public class CozinhaModelAssembler {
--- src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
+++ src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -2,7 +2,8 @@ package com.algaworks.algafood.api.model;
 
 import java.math.BigDecimal;
 
-import com.algaworks.algafood.domain.model.CozinhaModel;
+import com.algaworks.algafood.api.model.view.RestauranteView;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,9 +12,16 @@ import lombok.Setter;
 @Getter
 public class RestauranteModel {
 
+	@JsonView(value = {RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})
 	private Long id;
+	
+	@JsonView(value = {RestauranteView.Resumo.class, RestauranteView.ApenasNome.class})
 	private String nome;
+	
+	@JsonView(value = RestauranteView.Resumo.class)
 	private BigDecimal precoFrete;
+	
+	@JsonView(value = RestauranteView.Resumo.class)
 	private CozinhaModel cozinha;
 	private Boolean ativo;
 	private EnderecoModel endereco;
