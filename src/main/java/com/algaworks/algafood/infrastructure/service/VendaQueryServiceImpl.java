29748ac473101d014cf926ee728c1400e88a4808
Helio Dourado <heliohdd@yahoo.com.br>
2024-10-21 09:57:00 -0300

    13.15-desafio-adicionando-os-filtros-na-consulta-de-vendas-diarias

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.algaworks.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.api.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 
 @Repository
@@ -24,6 +27,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		var builder = entityManager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class);
 		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
 		
 		var functionDateDataCriacao = builder.function(
 				"date", Date.class, root.get("dataCriacao"));
@@ -33,7 +37,25 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 				builder.count(root.get("id")),
 				builder.sum(root.get("valorTotal")));
 
+		if (filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if (filtro.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThan(root.get("dataCriacao"), 
+					filtro.getDataCriacaoInicio()));
+		}
+		
+		if (filtro.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status").in(
+				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(functionDateDataCriacao);
 		
 		return entityManager.createQuery(query).getResultList();
