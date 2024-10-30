0e0c65e51b6542e76208f8570870432bddf686ed
Helio Dourado <heliohdd@yahoo.com.br>
2024-10-30 15:34:54 -0300

    16.09-desafio-implementando-uma-requisicao-delete-com-javascript

--- src/test/resources/algafood-js-client/client.js
+++ src/test/resources/algafood-js-client/client.js
@@ -28,7 +28,7 @@ function cadastrar() {
         },
 
         error: function (error) {
-            if(error.status == 400) {
+            if (error.status == 400) {
                 var problem = JSON.parse(error.responseText);
                 alert(problem.userMessage);
             } else {
@@ -38,15 +38,48 @@ function cadastrar() {
     });
 }
 
+function excluir(formaPagamento) {
+    var url = "http://api.algafood.local:8080/formas-pagamento/" + formaPagamento.id;
+
+    $.ajax({
+    url: url,
+    type: "delete",
+
+    success: function(response) {
+        consultar();
+
+        alert("Forma de pagamento removida!");
+    },
+
+    error: function(error) {
+        // tratando todos os erros da categoria 4xx
+        if (error.status >= 400 && error.status <= 499) {
+        var problem = JSON.parse(error.responseText);
+        alert(problem.userMessage);
+        } else {
+        alert("Erro ao remover forma de pagamento!");
+        }
+    }
+    });
+}
+
 function preencherTabela(formasPagamento) {
     $("#tabela tbody tr").remove();
 
-    $.each(formasPagamento, function (i, formaPagamento) {
+    $.each(formasPagamento, function(i, formaPagamento) {
         var linha = $("<tr>");
 
+        var linkAcao = $("<a href='#'>")
+            .text("Excluir")
+            .click(function (event) {
+                event.preventDefault();
+                excluir(formaPagamento);
+            });
+        
         linha.append(
             $("<td>").text(formaPagamento.id),
-            $("<td>").text(formaPagamento.descricao)
+            $("<td>").text(formaPagamento.descricao),
+            $("<td>").append(linkAcao)
         );
 
         linha.appendTo("#tabela");
@@ -54,4 +87,4 @@ function preencherTabela(formasPagamento) {
 }
 
 $("#btn-consultar").click(consultar);
-$("#btn-cadastrar").click(cadastrar)
+$("#btn-cadastrar").click(cadastrar);
